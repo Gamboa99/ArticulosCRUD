f8687f75df6005eac412228c950f3fa6b067c787
mpaulaGG <paulgamboa99@gmail.com>
2026-08-27 11:18:59 -0600

    Menu estatico

---
+++ Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticulosCRUD
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+
+        }
+
+        public void MostrarMenu()
+        {
+            bool continuar = true;
+            while (continuar) 
+            {
+                Console.Clear();
+                Console.WriteLine("Gestor de Articulos");
+                Console.WriteLine("===================");
+                Console.WriteLine("1. Agregar");
+                Console.WriteLine("2. Listar");
+                Console.WriteLine("3. Buscar");
+                Console.WriteLine("4. Modificar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("0. Salir");
+                string opcion = Console.ReadLine() ?? ""; 
+                switch(opcion) 
+                {
+                    case "0":
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción Invalida");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+          
+
+        }
+    }
+}
